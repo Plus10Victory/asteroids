c536a3a9678107dd5e03c828b6fcc4563736a700
Plus10Victory <plustenvictory@gmail.com>
2024-12-11 14:18:17 -0800

    game display created

--- main.py
+++ main.py
@@ -1,7 +1,21 @@
 import pygame
+from constants import *
 
 def main():
+    # initiate pygame
+    pygame.init()
+    # set screen dimensions
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    # game loop
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        pygame.Surface.fill(screen,(0,0,0))
+        pygame.display.flip()
     print("Starting asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
 
 if __name__ == "__main__":
     main()
