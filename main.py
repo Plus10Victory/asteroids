b14f4333814f8a3e7fe977a383b1a5b539498f7a
Plus10Victory <plustenvictory@gmail.com>
2024-12-11 14:38:47 -0800

    set FPS to 60

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from constants import *
 def main():
     pygame.init() # initiate pygame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # set screen dimensions
+    clock = pygame.time.Clock()
+    dt = 0
     # game loop
     while True:
         for event in pygame.event.get():
@@ -13,8 +15,10 @@ def main():
 
         screen.fill("black")
         pygame.display.flip()
+
+        # limit FPS to 60
+        dt = clock.tick(60) / 1000
     
 
 if __name__ == "__main__":
     main()
-    
