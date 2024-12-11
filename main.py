6cb45e213dfc5590edf2dd8d5608e71a9b68f69d
Plus10Victory <plustenvictory@gmail.com>
2024-12-11 15:09:17 -0800

    circleshape.py & player.py added, main updated

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import pygame
 from constants import *
+from player import Player
 
 
 def main():
     pygame.init() # initiate pygame
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) # set screen dimensions
     clock = pygame.time.Clock()
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
     # game loop
     while True:
@@ -14,6 +16,7 @@ def main():
                 return
 
         screen.fill("black")
+        player.draw(screen)
         pygame.display.flip()
 
         # limit FPS to 60
